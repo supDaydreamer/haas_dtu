355a01f22a661e49026dc744fa442dc6915cf605
Johy <zzzhyyy04301531@gmail.com>
2026-01-26 17:12:55 +0800

    新增socket线程门控，modbus_tcp_enable使能。

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <pthread.h>
 #include "common.h"
 #include "mqtt.h"
@@ -8,6 +9,7 @@
 #include "udp.h"
 #include "curl.h"
 #include "mbtcp_server.h"
+#include "ini.h"
 
 //int main(int argc, char **argv)
 int main()
@@ -29,6 +31,14 @@ int main()
 	pthread_t thread_socket;
 	pthread_t thread_mbtcp_server;
 //	pthread_t thread_udp_2;
+	int socket_thread_enabled = 0;
+	int modbus_tcp_enable = GetIniKeyInt("config", "modbus_tcp_enable", FILENAME);
+
+	if (modbus_tcp_enable == 1) {
+		socket_thread_enabled = 1;
+	} else {
+		printf("[MBTCP] modbus_tcp_enable=0, skip socket_main\n");
+	}
 
 	pthread_create(&thread_mqtt, NULL, mqtt_main, NULL);
 	pthread_create(&thread_haas_mqtt, NULL, haas_mqtt_main, NULL);
@@ -37,7 +47,9 @@ int main()
 	pthread_create(&thread_data, NULL, data_main, NULL);
 	pthread_create(&thread_cmd, NULL, cmd_main, NULL);
 //	pthread_create(&thread_yield, NULL, yield_main, NULL);
-	pthread_create(&thread_socket, NULL, socket_main, NULL);
+	if (socket_thread_enabled) {
+		pthread_create(&thread_socket, NULL, socket_main, NULL);
+	}
 	pthread_create(&thread_mbtcp_server, NULL, mbtcp_server_main, NULL);
 //	pthread_create(&thread_udp_2, NULL, udp_uart_main_2, NULL);
 
@@ -48,7 +60,9 @@ int main()
 	pthread_join(thread_data, NULL);
 	pthread_join(thread_cmd, NULL);
 //	pthread_join(thread_yield, NULL);
-	pthread_join(thread_socket, NULL);
+	if (socket_thread_enabled) {
+		pthread_join(thread_socket, NULL);
+	}
 	pthread_join(thread_mbtcp_server, NULL);
 //	pthread_join(thread_udp_2, NULL);
 
